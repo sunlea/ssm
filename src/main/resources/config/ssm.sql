cc9fd40f1cc6543a41ca8cf29b464e374ee181bf
Administrator <Administrator@PC-201705271131>
2017-07-12 10:43:53 +0800

    commit

---
+++ src/main/resources/config/ssm.sql
@@ -0,0 +1,30 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.0.96-community-nt 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `user` (
+	`id` varchar (150),
+	`userName` varchar (90),
+	`password` varchar (60),
+	`name` varchar (90),
+	`nickname` varchar (90),
+	`sex` varchar (30),
+	`picture` varchar (765),
+	`createtime` varchar (60),
+	`lastlogintime` varchar (150),
+	`tilepath` varchar (765),
+	`age` varchar (90)
+); 
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_00001','admin','admin888','BOONYA_U','BOONYA_*','男','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','20');
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_000010','张三111','123456','BOONYA_U','BOONYA_*','男','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','26');
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_00002','BOONYA','123456','BOONYA_U','BOONYA_*','女','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','21');
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_00003','BOONYA','123456','BOONYA_U','BOONYA_*','男','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','24');
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_00004','BOONYA','123456','BOONYA_U','BOONYA_*','男','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','26');
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_00005','BOONYA','123456','BOONYA_U','BOONYA_*','女','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','22');
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_00006','BOONYA','123456','BOONYA_U','BOONYA_*','男','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','20');
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_00007','BOONYA','123456','BOONYA_U','BOONYA_*','男','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','23');
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_00008','BOONYA','123456','BOONYA_U','BOONYA_*','男','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','25');
+insert into `user` (`id`, `userName`, `password`, `name`, `nickname`, `sex`, `picture`, `createtime`, `lastlogintime`, `tilepath`, `age`) values('HUSDHSHFSD_00009','BOONYA','123456','BOONYA_U','BOONYA_*','女','images/inde.png','2012-12-17 15:30:23','1832928398932','this/home/index.tile','26');
